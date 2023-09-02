4d094019d47437b0e98be5614eed59733eb02e7e
astutz96 <astutz96.development@protonmail.com>
2023-09-01 22:54:23 -0400

    fixing alignment of hero image and text details. The text details no longer cover/push the image off the screen

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -1,17 +1,17 @@
 package com.example.superheroes
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
@@ -30,12 +30,15 @@ fun SuperheroItem(
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
         Row(
+
             modifier = modifier
                 .fillMaxWidth()
                 .padding(dimensionResource(R.dimen.padding_medium))
-                .clip(Shapes.large)
+                .clip(Shapes.large),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Column {
+            Column(modifier = Modifier.weight(1f)) {
                 Text(
                     text = stringResource(superhero.name),
                     style = Typography.displaySmall,
@@ -45,11 +48,13 @@ fun SuperheroItem(
                     style = Typography.bodyLarge,
                 )
             }
-            Spacer(modifier = Modifier.width(dimensionResource(R.dimen.padding_medium)))
             Image(
                 modifier = modifier
                     .clip(Shapes.small)
-                    .size(dimensionResource(R.dimen.padding_X_large72)),
+                    .size(dimensionResource(R.dimen.padding_X_large72))
+                    .padding(
+                        start = dimensionResource(R.dimen.padding_medium)
+                    ),
                 painter = painterResource(id = superhero.imageResourceId),
                 contentDescription = null
                 //contentScale = ContentScale.Crop
